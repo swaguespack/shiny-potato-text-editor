410ea310c174316daecf38f381363dd6a27f67a5
shellby.miller <shellby.miller@gmail.com>
2023-03-20 19:27:16 -0400

    edits to webpack.config, src-sw.js, and database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,49 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Connect to database
+  const jateDb = await openDB('jate',1);
+
+  // Create transaction & set database and privileges
+  const tx = jateDb.transaction('jate','readwrite');
+
+  // Open object store
+  const store = tx.objectStore('jate');
+
+  // Pass in content to store using .add() method
+  const request = store.put({ id: 1, value: content });
+
+  // Confirmation of request
+  const result = await request;
+
+  console.log('Data saved to database', result);
+};
+
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+
+  // Connect to database
+  const jateDb = await openDB('jate', 1);
+
+  // Create transaction & set database and privileges
+  const tx = jateDb.transaction('jate','readonly');
+
+  // Open object store
+  const store = tx.objectStore('jate');
+
+  // Get all content using .get method
+  const request = store.get(1);
+
+   // Confirmation of request
+  const result = await request;
+  result
+  ? console.log('Data retrieved from database')
+  : console.log('Data not in databse');
+
+  return result?.value;
+};
 
 initdb();
